702987c3a311b298936abc288e2f2a4b0905ae3e
TC Wang <wangtct@hotmail.com>
2023-12-30 10:06:20 +0800

    system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxt/image-edge", "@vueuse/nuxt"],
+
+  app: {
+    head: {
+      title: "Nuxt3 Bootstrap - TopRestaurants",
+      meta: [
+        {
+          name: "description",
+          content: "Nuxt 3 Bootcamp Practice - TopRestaurants",
+        },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+});
